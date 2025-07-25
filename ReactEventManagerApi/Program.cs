adaa13032d937ac82a4cbe530beac1d0d444dd42
chiragk5000 <chiragk5000@gmail.com>
2025-07-25 18:35:15 +0530

    Commiting the code

--- ReactEventManagerApi/Program.cs
+++ ReactEventManagerApi/Program.cs
@@ -1,8 +1,11 @@
 using Application.Activities.Queries;
+using Application.Activities.Validator;
 using Application.Core;
+using FluentValidation;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ReactEventManagerApi.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,16 +15,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Get connection string from appsettings.json
+// Get connection string from appsettings.json 
 var connectionString = builder.Configuration.GetConnectionString("EventManagerDatabase");
 
 // Register DbContext with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddCors();
-builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>()); // for single 
+builder.Services.AddMediatR(x =>
+{
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>(); // for single 
+    x.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+});
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValdiator>(); // for fluent valdiator 
 //builder.Services.AddMediatR(AppDomain.CurrentDomain.Load("Application"));
+builder.Services.AddTransient<ExceptionMiddleware>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,8 +44,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-            .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+            .WithOrigins("https://localhost:3000"));
 app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
